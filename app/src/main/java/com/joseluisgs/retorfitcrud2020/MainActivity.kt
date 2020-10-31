241bf4792db0f9575a5878fa2bcb644506049607
joseluisgs <joseluisgonsan@gmail.com>
2020-10-31 10:34:09 +0100

    Botones Main Texto

--- app/src/main/java/com/joseluisgs/retorfitcrud2020/MainActivity.kt
+++ app/src/main/java/com/joseluisgs/retorfitcrud2020/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.joseluisgs.retorfitcrud2020
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +21,13 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Sí estás conectado a Internet", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "Es necesaria una conexión a internet para funcionar", Toast.LENGTH_SHORT).show();
+        }
+
+        // Probamos un botón para abri la actividad
+        mainBtnAñadir.setOnClickListener {
+            val intent = Intent(this, UsuarioActivity::class.java)
+            // Comenzamos la actividad
+            startActivity(intent)
         }
     }
 
