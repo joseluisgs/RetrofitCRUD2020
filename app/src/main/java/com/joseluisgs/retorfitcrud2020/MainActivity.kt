299b8c1fd910717d28d78631dd4ef9efe88cd23d
joseluisgs <joseluisgonsan@gmail.com>
2020-10-31 09:57:23 +0100

    Permisos y conexión a internet

--- app/src/main/java/com/joseluisgs/retorfitcrud2020/MainActivity.kt
+++ app/src/main/java/com/joseluisgs/retorfitcrud2020/MainActivity.kt
@@ -1,11 +1,26 @@
- package com.joseluisgs.retorfitcrud2020
+package com.joseluisgs.retorfitcrud2020
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initUI()
     }
+
+    private fun initUI() {
+        // Comprobamos la conexión a internet
+        if (Utils.isOnline(applicationContext)) {
+            Toast.makeText(this, "Sí estás conectado a Internet", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Es necesaria una conexión a internet para funcionar", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
