47f58b6dc51db793f93e844409ca2917ca3cd51a
joseluisgs <joseluisgonsan@gmail.com>
2020-10-31 10:58:00 +0100

    Usuario Modelo

---
+++ app/src/main/java/com/joseluisgs/retorfitcrud2020/modelos/usuarios/Usuario.kt
@@ -0,0 +1,9 @@
+package com.joseluisgs.retorfitcrud2020.modelos.usuarios
+
+data class Usuario(
+    val id: String?,
+    val email: String?,
+    val firstName: String?,
+    val lastName: String?,
+    val avatar: String?,
+)
